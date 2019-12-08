e062e48ad28bee774e7a85955867731398235c63
hichay <babykinglovegirlcry@gmail.com>
2019-12-09 04:48:10 +0700

    Update LuaSimpleXorEncrypt.js

--- src/LuaSimpleXorEncrypt.js
+++ src/LuaSimpleXorEncrypt.js
@@ -6,15 +6,7 @@ import templates from './templates';
 
 function luaSimpleXorEncrypt(bytes, key, options = {}) {
   let encrypted = simpleXorEncrypt(bytes, utf8.encode(key));
-  let shuffled = shuffleWithKey(encrypted, key);
-  let code = (options.isGG ? templates.keyInputCodeGG : templates.keyInputCode)
-    + templates.main
-    + (options.isLua52 ? templates.load : templates.loadstring)
-    + templates.decoder
-    + shuffled.join(',')
-    + templates.decoderEnd
-    + (options.isGG ? templates.keyWrongAlertCodeGG : templates.keyWrongAlertCode)
-    + templates.keyWrongAlertEnd;
+
   return templates.credit
     + luamin.minify(code);
 }
