0039c9794b702d4112778c2e5452b216d9171954
hichay <babykinglovegirlcry@gmail.com>
2019-12-09 04:55:28 +0700

    Update LuaSimpleXorEncrypt.js

--- src/LuaSimpleXorEncrypt.js
+++ src/LuaSimpleXorEncrypt.js
@@ -1,8 +1,7 @@
 import utf8 from 'utf8';
 import luamin from 'luamin';
 import simpleXorEncrypt from './SimpleXorEncrypt';
-import shuffleWithKey from './ShuffleWithKey';
-import templates from './templates';
+
 
 function luaSimpleXorEncrypt(bytes, key, options = {}) {
   let encrypted = simpleXorEncrypt(bytes, utf8.encode(key));
